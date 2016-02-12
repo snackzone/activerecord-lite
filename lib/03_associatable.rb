6b22c9a2a5a579ef9ad684df7ca1ebcce19cabf0
Zachary Moroni <zachary.moroni@gmail.com>
2016-02-12 12:26:05 -0500

    Clean up comments, refactor Has_one_through

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -1,8 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
-require 'byebug'
 
-# Phase IIIa
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -37,7 +35,6 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
     assoc_options[name] = options
@@ -58,7 +55,7 @@ module Associatable
       options.model_class.new(results.first)
     end
   end
-  #
+  
   def has_many(name, options = {})
     options = HasManyOptions.new(name, self.table_name, options)
 
@@ -79,18 +76,35 @@ module Associatable
     end
   end
 
-  def has_one_through(name, through_name, source_method)
-    through_values = assoc_options[through_name]
-    through_options = BelongsToOptions.new(through_name, through_values)
-
-    source_values = through_options.model_class.assoc_options[source_method]
-    source_options = BelongsToOptions.new(source_method, source_values)
-
-
-    raise "error" if through_options.nil?
-
+  def has_one_through(name, through_name, source_name)
     define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options =
+        through_options.model_class.assoc_options[source_name]
 
+      through_table = through_options.table_name
+      through_pk = through_options.primary_key
+      through_fk = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_pk = source_options.primary_key
+      source_fk = source_options.foreign_key
+
+      key_val = self.send(through_fk)
+      results = DBConnection.execute(<<-SQL, key_val)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_fk} = #{source_table}.#{source_pk}
+        WHERE
+          #{through_table}.#{through_pk} = ?
+      SQL
+
+      source_options.model_class.parse_all(results).first
     end
   end
 
