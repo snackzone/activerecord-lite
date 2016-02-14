aeee4224f6e95fa6cda3496857482f3696bbecce
Zachary Moroni <zachary.moroni@gmail.com>
2016-02-14 12:08:06 -0500

    Has many through.

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 class AssocOptions
   attr_accessor(
@@ -9,7 +10,7 @@ class AssocOptions
   )
 
   def model_class
-    Object.const_get(class_name)
+    class_name.constantize
   end
 
   def table_name
@@ -27,10 +28,10 @@ end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
+    debugger
     @primary_key = options[:primary_key] || :id
-    @foreign_key = options[:foreign_key] ||
-                    "#{self_class_name.downcase.singularize}_id".to_sym
-    @class_name = options[:class_name] || name.to_s.capitalize.singularize
+    @foreign_key = options[:foreign_key] || "#{self_class_name.to_s.underscore}_id".to_sym
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
@@ -55,9 +56,11 @@ module Associatable
       options.model_class.new(results.first)
     end
   end
-  
+
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, self.table_name, options)
+    debugger
+    options = HasManyOptions.new(name, self.to_s, options)
+    assoc_options[name] = options
 
     define_method(name) do
       target_key_value = self.send(options.primary_key)
@@ -76,9 +79,14 @@ module Associatable
     end
   end
 
+  def assoc_options
+    @assoc_options ||= {}
+  end
+
   def has_one_through(name, through_name, source_name)
+    through_options = assoc_options[through_name]
+
     define_method(name) do
-      through_options = self.class.assoc_options[through_name]
       source_options =
         through_options.model_class.assoc_options[source_name]
 
@@ -99,7 +107,7 @@ module Associatable
         JOIN
           #{source_table}
         ON
-          #{through_table}.#{source_fk} = #{source_table}.#{source_pk}
+          #{source_table}.#{source_fk} = #{through_table}.#{source_pk}
         WHERE
           #{through_table}.#{through_pk} = ?
       SQL
@@ -108,9 +116,38 @@ module Associatable
     end
   end
 
-  def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
-    @assoc_options ||= {}
+  def has_many_through(name, through_name, source_name)
+    debugger
+    through_options = assoc_options[through_name]
+    define_method(name) do
+      debugger
+      source_options =
+        through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_pk = through_options.primary_key
+      through_fk = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_pk = source_options.primary_key
+      source_fk = source_options.foreign_key
+
+      key_val = self.send(through_pk)
+      results = DBConnection.execute(<<-SQL, key_val)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{source_table}
+        JOIN
+          #{through_table}
+        ON
+          #{through_table}.#{through_pk} = #{source_table}.#{source_fk}
+        WHERE
+          #{through_table}.#{through_fk} = ?
+      SQL
+
+      source_options.model_class.parse_all(results)
+    end
   end
 end
 
